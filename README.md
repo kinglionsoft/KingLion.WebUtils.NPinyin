06f5ea9d374da7b47d688d0dec37d4943eefb6e2
yangchao <yangch_ts@qq.com>
2016-12-05 20:15:09 +0800

    创建库

---
+++ README.md
@@ -0,0 +1,8 @@
+# KingLion.WebUtils.NPinyin
+
+代码完全来自 [https://code.google.com/p/npinyin](https://code.google.com/p/npinyin)，仅使用.Net Core将其重新编译。
+
+>**关于版权**
+>
+>本项目的许可证为MIT。由于网络原因，无法看到原项目的许可证；若原项目的许可与本项目冲突，**以原项目的许可范围为准**。
+
---
+++ src/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(KingLion.WebUtils.NPinyin.Pinyin.GetPinyin("中华人民共和国","-"));
+            Console.Read();
+        }
+    }
+}
